af7960fcdc7ca85b7ff65e4de5adbb92fd3d0b1e
TheHoboCoder <animator.x@yandex.ru>
2022-04-15 16:26:02 +0300

    added display validation errors

--- Old Site/mchs/js/lavina/authorization.js
+++ Old Site/mchs/js/lavina/authorization.js
@@ -1,5 +1,16 @@
 let current_user = null;
 
+// добавляет после каждого поля ввода в форме регистрации
+// строчку для вывода ошибок валидации этого поля
+$("#reg-form input[type!=button]").each(function(index){
+    $("<div>", {
+        'class': 'error-message',
+        'id': $(this).attr("name") + "-error-msg",
+        'style': "display: none;"
+    })
+      .insertAfter($(this).next());
+});
+
 tryFetchCurrentUser(onUserLoad, onLogout);
 
 $("#vixod").click(function(){
@@ -35,6 +46,7 @@ function onLogout(data){
 $("#register-submit-btn").click(function(){
     register($('#reg-form').serialize(), function(data){
         console.log(data);
+        $("#reg-form .error-message").hide();
         login($('#reg-form')
                 .find("input[name=username], input[name=password]").serialize(),
                 () => tryFetchCurrentUser(onUserLoad));
@@ -42,15 +54,28 @@ $("#register-submit-btn").click(function(){
     }, function(data){
         console.log("register failed!");
         console.log(data);
+        if(data.responseJSON != undefined){
+            for(const [key, value] of Object.entries(data.responseJSON)){
+                $(`#${key}-error-msg`).text(value.join("<br>")).show();
+            }
+        }
     });
 });
 
 $("#login-submit-btn").click(function () {
     login($('#login-form').serialize(), function (data) {
         console.log(data);
+        $("#login-error").hide();
         tryFetchCurrentUser(onUserLoad);
         $("#overlay").hide();
     }, function(data){
+        if(data.responseJSON != undefined){
+            let errorMsg = "Ошибка сервера";
+            if(data.responseJSON.detail == "Invalid credentials."){
+                errorMsg = "Неверный логин или пароль";
+            }
+            $("#login-error").text(errorMsg).show();
+        }
         console.log("login failed");
         console.log(data);
     });
